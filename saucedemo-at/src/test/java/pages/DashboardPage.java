694195b2e60c9489d5a790c05006a039834075b9
alexandrameghesan <meghesanalexandra@gmail.com>
2025-07-27 15:12:37 +0300

    added logs to the pages

--- saucedemo-at/src/test/java/pages/DashboardPage.java
+++ saucedemo-at/src/test/java/pages/DashboardPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,9 +9,14 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.TestUtil;
 
 import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+
 import java.util.stream.Collectors;
 
 public class DashboardPage extends AbstractPageFactory {
+
+    private static final Logger logger = LogManager.getLogger(DashboardPage.class);
 
     @FindBy(xpath = "//div[@class='app_logo']")
     private WebElement dashboardTitle;
@@ -38,13 +45,13 @@ public class DashboardPage extends AbstractPageFactory {
     @FindBy(id = "remove-sauce-labs-bike-light")
     private WebElement removeButtonForBikeLight;
 
-    @FindBy(id="continue-shopping")
+    @FindBy(id = "continue-shopping")
     private WebElement continueShoppingButton;
 
-    @FindBy(id="add-to-cart-sauce-labs-onesie")
+    @FindBy(id = "add-to-cart-sauce-labs-onesie")
     private WebElement onesieItem;
 
-    @FindBy(id="checkout")
+    @FindBy(id = "checkout")
     private WebElement checkoutButton;
 
     public DashboardPage(WebDriver driver) {
@@ -53,54 +60,83 @@ public class DashboardPage extends AbstractPageFactory {
     }
 
     @Override
-    public DashboardPage openPage(String url ){
+    public DashboardPage openPage(String url) {
+        logger.info("Opening the page with url: {}", url);
         getDriver().get(url);
+        logger.debug("The page was opened successfully");
         return this;
     }
 
     public String getDashboardTitle() {
-        TestUtil.waitForVisibility(getDriver(), dashboardTitle, 3);
-        return dashboardTitle.getText();
+        try {
+            TestUtil.waitForVisibility(getDriver(), dashboardTitle, 3);
+            String title = dashboardTitle.getText();
+            logger.info("Dashboard title found: {}", title);
+            return title;
+        } catch (Exception exception) {
+            logger.error("Dashboard title element not visible within timeout", exception);
+            throw exception;
+        }
     }
 
 
     public DashboardPage addItemsToCart() {
-        backPackItem.click();
-        bikeLightItem.click();
-        fleeceJacketItem.click();
-        boltTShirtItem.click();
+        try {
+            backPackItem.click();
+            logger.info("Backpack item added to cart.");
+
+            bikeLightItem.click();
+            logger.info("Bike light item added to cart.");
+
+            fleeceJacketItem.click();
+            logger.info("Fleece jacket item added to cart.");
+
+            boltTShirtItem.click();
+            logger.info("Bolt t-shirt item added to cart.");
+        } catch (Exception e) {
+            logger.error("Failed to add items to cart", e);
+            throw e;
+        }
         return this;
     }
 
     public DashboardPage goToCart() {
         cartIcon.click();
+        logger.info("Navigating to cart page.");
         return this;
     }
 
     public List<String> getCartItemsNames() {
-        return cartItems.stream()
+        List<String> itemNames = cartItems.stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
+        logger.info("Items currently in the cart: {}", itemNames);
+        return itemNames;
     }
 
     public DashboardPage removeItems() {
+        logger.info("Removing backpack item from cart.");
         removeButtonForBackPackItem.click();
+        logger.info("Removing bike light item from cart.");
         removeButtonForBikeLight.click();
         return this;
     }
 
     public DashboardPage goBackToItems() {
         continueShoppingButton.click();
+        logger.info("Clicking 'Continue Shopping' button.");
         return this;
     }
 
     public DashboardPage addOnesieItemToCart() {
         onesieItem.click();
+        logger.info("Adding onesie item to cart.");
         return this;
     }
 
     public DashboardPage goToCheckout() {
         checkoutButton.click();
+        logger.info("Proceeding to checkout page.");
         return this;
     }
 }
--- saucedemo-at/src/test/java/pages/LoginPage.java
+++ saucedemo-at/src/test/java/pages/LoginPage.java
@@ -1,5 +1,8 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,6 +11,8 @@ import utilities.TestUtil;
 
 
 public class LoginPage extends AbstractPageFactory {
+
+        private static final Logger logger = LogManager.getLogger(LoginPage.class);
 
         @FindBy(xpath = "//input[@id='user-name']")
         private WebElement usernameInput;
@@ -26,27 +31,48 @@ public class LoginPage extends AbstractPageFactory {
 
         @Override
         public LoginPage openPage(String url) {
+            logger.info("Opening the page with url: {}", url);
             getDriver().get(url);
+            logger.debug("The page was opened successfully");
             return this;
         }
 
 
         public LoginPage enterUsername(String usernameValue) {
-            usernameInput.sendKeys(usernameValue);
+            try {
+                logger.info("Entering username: {}", usernameValue);
+                usernameInput.sendKeys(usernameValue);
+            }catch(Exception exception) {
+                logger.error("Username field not visible or interactable.", exception);
+                throw exception;
+            }
+
             return this;
         }
 
 
         public LoginPage enterPassword(String passwordValue) {
-            passwordInput.sendKeys(passwordValue);
+            try {
+                logger.info("Entering password: [hidden]");
+                passwordInput.sendKeys(passwordValue);
+            } catch (TimeoutException exception) {
+                logger.error("Password field not visible or interactable.", exception);
+                throw exception;
+            }
             return this;
         }
 
         public LoginPage clickLoginButton() {
-        loginButton.click();
-        return this;
+            try {
+                logger.info("Clicking login button.");
+                loginButton.click();
+            } catch (TimeoutException exception) {
+                logger.error("Login button not clickable or timed out.", exception);
+                throw exception;
+            }
+            logger.debug("Login button clicked successfully.");
+            return this;
         }
-
     }
 
 
